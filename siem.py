9a67850569ec24cf78b9fae810b62cf00cf5115c
tancque <31699610+tancque@users.noreply.github.com>
2024-09-05 15:20:03 +0200

    Update siem.py
    Beter fileafhandeling, extra foutafhandeling, en verbeterde snelheid database inserts.

--- siem.py
+++ siem.py
@@ -8,18 +8,33 @@ import glob
 import re
 import shutil
 import smtplib
+import subprocess
+import traceback
+import os
 from email.message import EmailMessage
 from datetime import date,datetime
 
-conn = psycopg2.connect(host='localhost',database='siem',user='siem',password='***********')
+conn = psycopg2.connect(host='localhost',database='siem',user='siem',password='**********')
 emailmessage = "Hier de dagelijkse analyze van de snmplogs : \n\n\n\n"
+debugmessage = "Hier de dagelijkse analyze in debug : \n\n\n\n"
 
-# indexeer alle beschikbare logfiles van gister
+# indexeer alle beschikbare logfilesi van gister
 gloober = "/var/log/network-*.log"
 baseid = 0
 
+#  sommige servers spuwen zoveel log dat het script het bewerken van een running log
+# niet bijhoud. Dus eerst moven en dan pas inlezen...
+
 listallelogfiles = glob.glob(gloober)
-for logfile in listallelogfiles:
+for origineel in listallelogfiles:
+         shutil.move(origineel, "/var/log/siem-archive/")
+subprocess.call(["systemctl", "restart", "rsyslog"])
+
+gloober2 = "/var/log/siem-archive/network-*.log"
+listalleteverwerken = glob.glob(gloober2)
+
+
+for logfile in listalleteverwerken:
                 print (str(logfile))
                 host=re.search("\d+\.\d+\.\d+\.\d+",logfile).group(0)
                 hosttabelnaam="a" + host.replace(".","_")
@@ -52,6 +67,7 @@ for logfile in listallelogfiles:
 
                 LOGFILE=open(logfile,"r")
                 for line in LOGFILE:
+                        try:
                                 # vind service en eventid
                                 infopart=re.search("#011.*#015", line).group(0)
                                 infoparts = infopart.split("#011")
@@ -61,8 +77,14 @@ for logfile in listallelogfiles:
                                 insertquery = 'INSERT INTO ' +  hosttabelnaam +  ' (service, eventid, datum,  message) VALUES (\'' + infoparts[6] + '\', \'' + infoparts[5] + '\', \'' + infoparts[4] + '\', \'' + geschoondelijn + '\')'
                                 insertcursor = conn.cursor()
                                 insertcursor.execute(insertquery)
-                                conn.commit()
-                                insertcursor.close()
+                        except Exception as e:
+                                  debugmessage = debugmessage + str(traceback.format_exc())
+                                  print(str(traceback.format_exc()))
+
+                # commit verhuisd na hele actie niet na elke insert
+                # minder veilig, wel veel sneller
+                conn.commit()
+                insertcursor.close()
 
 
                 # logfile verwerkt, start analyze
@@ -72,7 +94,10 @@ for logfile in listallelogfiles:
                 meldingtupiescursor.execute("Select distinct service,eventid from " +  hosttabelnaam + " where id>" + str(baseid))
                 # print ("tupies-query : Select distinct service,eventid from " +  hosttabelnaam + " where id>" + str(baseid))
 
-          
+                debugmessage = debugmessage + "\n hostname : " + host
+
+                if baseid==0:
+                        baseid = 0.0001
 
                 for (service,evenid) in meldingtupiescursor:
                         # bereken basisfrequentie van deze meldingen
@@ -94,7 +119,9 @@ for logfile in listallelogfiles:
                         freqcursor.close()
 
                         #print (service + ": " + str(evenid) + " : current freq = " + str(currentfreq) + " : normal freq = " + str(totalfreq))
-                       
+                        debugmessage = debugmessage + "\n" + service + ": " + str(evenid) + " : current freq = " + str(currentfreq) + " : normal freq = " + str(totalfreq)
+
+
                         if currentfreq > (1.98 * totalfreq):
                                 meldingen[service] = evenid
 
@@ -105,17 +132,28 @@ for logfile in listallelogfiles:
                                            emailmessage = emailmessage + "\n" + str(key) + ", " + str(meldingen[key])
 
                 destlogfile = logfile[9:] + str(datetime.now())[:10]
-                shutil.move(logfile, "/var/log/siem-archive/" + destlogfile)
+                os.rename(logfile, "/var/log/" + destlogfile)
 
 conn.close()
+
+
 msg = EmailMessage()
 msg.set_content(emailmessage)
 msg['Subject'] = "logfile analyse"
-msg['From'] = "server@institute.org"
-msg['To'] = "sysadmin@institute.org"
+msg['From'] = "server@ivdnt.loc"
+msg['To'] = "systeembeheer@ivdnt.org"
 
-s = smtplib.SMTP('mailserver')
+s = smtplib.SMTP('aa.bb.cc.dd')
 s.send_message(msg)
 s.quit()
 
 
+# msg2 = EmailMessage()
+#msg2.set_content(debugmessage)
+#msg2['Subject'] = "logfile analyse (debug)"
+#msg2['From'] = "server@ivdnt.loc"
+#msg2['To'] = "systeembeheer@ivdnt.org"
+
+#s = smtplib.SMTP('aa.bb.cc.dd')
+#s.send_message(msg2)
+#s.quit()
